445d6db056477b65032b2d35190da1a8a402ff92
Felipe Bonezi <felipebonezi@gmail.com>
2014-05-29 19:18:28 -0300

    Model
    Adicionado o metodo equals para checar através de outros estados

--- src/br/com/fugb/ecomp/dijkstra/models/Vertex.java
+++ src/br/com/fugb/ecomp/dijkstra/models/Vertex.java
@@ -1,5 +1,6 @@
 package br.com.fugb.ecomp.dijkstra.models;
 
+
 /**
  * Classe responsável por guardar a referência do Vértice utilizado no Grafo.
  * @author Felipe Bonezi <felipebonezi@gmail.com>
@@ -14,6 +15,27 @@ public class Vertex {
 	
 	public Vertex(String identifier) {
 		this.identifier = identifier;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		
+		if (obj == null)
+			return false;
+		
+		if (getClass() != obj.getClass())
+			return false;
+		
+		Vertex other = (Vertex) obj;
+		if (identifier == null) {
+			if (other.identifier != null)
+				return false;
+		} else if (!identifier.equals(other.identifier))
+			return false;
+		
+		return true;
 	}
 	
 	@Override
